f512e6526713c5599d05b5676a9130591c150a5d
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-07-21 12:44:31 -0400

    I did A LOT of merging omg

--- main.js
+++ main.js
@@ -1,23 +1,40 @@
 function main() {
-$("about").hide();
-$(".html").hide();
-$(".html").fadeIn(1000);
-$(".topbar").hide();
-$(".topbar").fadeIn(2500);
-$(".topbar").on('click', function() {
-    $(".topbar").hide(1000);
+// alert("JS is alive!");
+$(".aboutbody").hide();
+$(".newsletterbody").hide();
+
+$(".about").on('click', function() {
+    $(".about").addClass("activelink");
+    $(".index").removeClass("activelink");
+    $(".newsletter").removeClass("activelink");
+    $(".aboutbody").show();
+    $(".homebody").hide();
+    $(".newsletterbody").hide();
   });
-$(".changepage").on('click', function() {
-   $(".div1").slideToggle(1000);
-   $(".div2").slideToggle(1000);
-   $(".topbar").hide(1000);
+$(".index").on('click', function() {
+    $(".index").addClass("activelink");
+    $(".about").removeClass("activelink");
+    $(".newsletter").removeClass("activelink");
+    $(".homebody").show();
+    $(".aboutbody").hide();
+    $(".newsletterbody").hide();
+  });
+$(".newsletter").on('click', function() {
+    $(".newsletter").addClass("activelink");
+    $(".about").removeClass("activelink");
+    $(".index").removeClass("activelink");
+    $(".newsletterbody").show();
+    $(".aboutbody").hide();
+    $(".homebody").hide();
   });
 }
 $(document).ready(main);
 
 
 
-
+// $(".topbar").on('click', function() {
+//     $(".topbar").hide(1000);
+//   });
 
 
 
