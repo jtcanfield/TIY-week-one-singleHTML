f5669f8503a513e55d29bb1dbe4f33cf3bf01470
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-07-21 14:36:28 -0400

    cleaned up files, unneeded notes

--- main.js
+++ main.js
@@ -45,18 +45,7 @@ signupButton.addEventListener('click', function(e) {
 }
 $(document).ready(main);
 
-
-
-// $(".topbar").on('click', function() {
-//     $(".topbar").hide(1000);
-//   });
-
-
-
-/* im definately not about to do some js stuff
-<script src='/main.js'></script>
-<script src='https://code.jquery.com/jquery-3.1.0.min.js'></script>
-
+/*
 This belongs in footer under Copyright:
 <p id="clickme">click me...</p>
 
