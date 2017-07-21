4ec42a5a9fe8a86dd9c6fd99ff8a67ae654b333e
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-07-21 11:30:37 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,18 @@
+/* im definately not about to do some js stuff
+<script src='/main.js'></script>
+<script src='https://code.jquery.com/jquery-3.1.0.min.js'></script>
+
+This belongs in footer under Copyright:
+<p id="clickme">click me...</p>
+
+
+This Belongs in CSS
+#clickme {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  opacity: 0;
+}
+#clickme:hover {
+  opacity: 1;
+}
